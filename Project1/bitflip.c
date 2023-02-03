9325bf7c1ef43c78a3ddf05424a1cff57d26e2d1
Mitchell Trossen <mtrosse2@nd.edu>
2023-02-02 22:16:32 -0500

    Working Version Level 3

--- Project1/bitflip.c
+++ Project1/bitflip.c
@@ -7,10 +7,10 @@
 
 //Function to return the file size
 int get_file_size(FILE *fp);
-void print_regular(char *fo_name, char *buf);
-void print_reverse(char *fo_name, char *buf);
+void print_regular(char *fo_name, char *buf, int come_from_dir);
+void print_reverse(char *fo_name, char *buf, int come_from_dir);
 void usage();
-void print_reverse_noflip(char *fo_name, char *buf);
+void print_reverse_noflip(char *fo_name, char *buf, int come_from_dir);
 void dir_main(char* dir_name, int max, int r_and_b, int reverse);
 
 int main(int argc, char *argv[]){
@@ -25,12 +25,12 @@ int main(int argc, char *argv[]){
 	int dir_inputed = 0;
 	int dir_idx = -1;
 
-	//Check command line input
+	// Check command line input to make sure there are enough
 	if(argc < 2){
 		printf("Wrong number of command line arguments\n");
 		return(3);
 	}
-
+	// parse command line arguments
 	for(i = 1; i < argc; i++){
 		if (strcmp(argv[i], "-help") == 0){
 			usage();
@@ -80,8 +80,8 @@ int main(int argc, char *argv[]){
 
 		} else if (strcmp(argv[i], "-bfr") == 0) {
 			r_and_b = 1; 
-		
-		} else if (strlen(argv[i]) > 4 && argv[i][strlen(argv[i])-4] == '.' && argv[i][strlen(argv[i])-3] == 't' && argv[i][strlen(argv[i])-2] == 'x' && argv[i][strlen(argv[i])-1] == 't') {
+		// make sure file ends in .txt, .r, .bfr, .bf
+		} else if ((strlen(argv[i]) > 4 && argv[i][strlen(argv[i])-4] == '.' && argv[i][strlen(argv[i])-3] == 't' && argv[i][strlen(argv[i])-2] == 'x' && argv[i][strlen(argv[i])-1] == 't') || (strlen(argv[i]) > 4 && argv[i][strlen(argv[i])-4] == '.' && argv[i][strlen(argv[i])-3] == 'b' && argv[i][strlen(argv[i])-2] == 'f' && argv[i][strlen(argv[i])-1] == 'r') || (strlen(argv[i]) > 2 && argv[i][strlen(argv[i])-2] == '.' && argv[i][strlen(argv[i])-1] == 'r') || (strlen(argv[i]) > 3 && argv[i][strlen(argv[i])-3] == '.' && argv[i][strlen(argv[i])-2] == 'b' && argv[i][strlen(argv[i])-1] == 'f')) {
 
 			infile_idx = i;
 
@@ -91,6 +91,7 @@ int main(int argc, char *argv[]){
 			found = 1;
 			
 		}
+		// open directory if none of the other flags work
 		else {
 			DIR *dir = opendir(argv[i]);
 			if(dir != NULL){
@@ -100,7 +101,8 @@ int main(int argc, char *argv[]){
 			}
 		}
 		
-
+	
+	// we need to tell where the function calls are coming from the directory calls or the normal calls. This tells us why
 	}
 	if(dir_inputed == 1){
 		dir_main(argv[dir_idx], max, r_and_b, reverse);
@@ -124,10 +126,8 @@ int main(int argc, char *argv[]){
 
 
 	// Check the size
-	// printf("Input: %s ", argv[infile_idx]);
 	int size = get_file_size(fp);
 	
-	//printf("3\n");
 	
 	if(size > max){
 		printf("File is too large\n");
@@ -142,24 +142,22 @@ int main(int argc, char *argv[]){
 	}
 	
 	
-	//change this to file)name
-
-	
+	// depending on which flags were set in the command line arguments, call the respective function call
 	if(r_and_b == 0 && reverse == 0) {
-		print_regular(argv[outfile_idx], buf);
+		print_regular(argv[outfile_idx], buf, 0);
 		printf("Input: %s is %d bytes\n", argv[infile_idx],size);	
 		printf("Output: %s.bf was output successfully (bitflip)\n", argv[outfile_idx]);
 	}
 
 
 	if(reverse == 1) {
-		print_reverse_noflip(argv[outfile_idx], buf);	
+		print_reverse_noflip(argv[outfile_idx], buf, 0 );	
 		printf("input: %s is %d bytes\n", argv[infile_idx],size);
 		printf("Output: %s.r was output successfully (reverse, no bitflip)\n ", argv[outfile_idx]);
 	}
 
 	if(r_and_b == 1) {
-		print_reverse(argv[outfile_idx], buf);	
+		print_reverse(argv[outfile_idx], buf, 0);	
 		printf("input: %s is %d bytes\n", argv[infile_idx],size);
 		printf("Output: %s.bfr was output successfully (reverse, bitflip)\n ",argv[outfile_idx]);
 	}
@@ -194,8 +192,8 @@ void usage(){
 	printf("-r		Reverse the byte order without doing bitflip\n");
 }
 
-
-void print_regular(char *fo_name, char *buf){
+// default bit flip function
+void print_regular(char *fo_name, char *buf, int come_from_dir){
 		
 	char *names = malloc(strlen(fo_name)+3*sizeof(char));
 	
@@ -203,17 +201,19 @@ void print_regular(char *fo_name, char *buf){
 	for(i=0; i<strlen(fo_name); i++){
 		names[i] = fo_name[i];
 	}
-
+	// add the respective ending
 	names[strlen(fo_name)] = '.';
 	names[strlen(fo_name)+1] = 'b';
 	names[strlen(fo_name)+2] = 'f';
-	
-	// check to see output file already exists without -o flag
+	names[strlen(fo_name)+3] = '\0';
+
 	FILE *ftemp = fopen(names, "r");
 	if(ftemp != NULL){
 			printf("Error: %s already exists!\n", names);
 			fclose(ftemp);
-			exit(9);
+			if(come_from_dir == 0){
+				exit(9);
+			}
 	}
 	FILE *fn = fopen(names, "w");
 	char temp;
@@ -225,13 +225,12 @@ void print_regular(char *fo_name, char *buf){
 	}
 
 	fclose(fn);	
-	// printf("Output: %s was output successfully (bitflip)\n", names);
 	free(names);
 }
 
 
-
-void print_reverse(char *fo_name, char *buf){
+// this function flips bit and reverses. called from r_and_b flag
+void print_reverse(char *fo_name, char *buf, int come_from_dir){
 		
 	char *names = malloc(strlen(fo_name)+4*sizeof(char));
 	
@@ -239,24 +238,24 @@ void print_reverse(char *fo_name, char *buf){
 	for(i=0; i<strlen(fo_name); i++){
 		names[i] = fo_name[i];
 	}
-
+	// add respective ending
 	names[strlen(fo_name)] = '.';
 	names[strlen(fo_name)+1] = 'b';
 	names[strlen(fo_name)+2] = 'f';
 	names[strlen(fo_name)+3] = 'r';
+	names[strlen(fo_name)+4] = '\0';
 
 	// check to see output file already exists without -o flag
 	FILE *ftemp = fopen(names, "r");
 		if(ftemp != NULL){
 			printf("Error: %s already exists!\n", names);
 			fclose(ftemp);
-			exit(9);
 		}
 
 
 	FILE *fn = fopen(names, "w");
 	char temp;
-
+	// traverse backware and bit flip to get desired result
 	for(i=strlen(buf)-1; i>=0; i--){
 		temp = buf[i];
 		temp = temp ^ 0xFF;
@@ -264,7 +263,6 @@ void print_reverse(char *fo_name, char *buf){
 	}
 
 	fclose(fn);	
-	// printf("Output: %s was output successfully (bitflip, reverse)\n", names); 
 	free(names);
 }
 
@@ -332,7 +330,9 @@ void print_reverse_noflip(char *fo_name, char *buf){
 
 */
 
-void print_reverse_noflip(char *fo_name, char *buf){
+
+// only reverse, don't flip
+void print_reverse_noflip(char *fo_name, char *buf, int come_from_dir){
 		
 	char *names = malloc(strlen(fo_name)+2*sizeof(char));
 	
@@ -340,9 +340,10 @@ void print_reverse_noflip(char *fo_name, char *buf){
 	for(i=0; i<strlen(fo_name); i++){
 		names[i] = fo_name[i];
 	}
-
+	// add respective ending
 	names[strlen(fo_name)] = '.';
 	names[strlen(fo_name)+1] = 'r';
+	names[strlen(fo_name)+2] = '\0';
 	
 	// check to see output file already exists without -o flag
 	FILE *ftemp = fopen(names, "r");
@@ -355,19 +356,18 @@ void print_reverse_noflip(char *fo_name, char *buf){
 
 	FILE *fn = fopen(names, "w");
 	char temp;
-
+	// do reverse
 	for(i=strlen(buf)-1; i >= 0; i--){
 		temp = buf[i];
 		fwrite(&temp, sizeof(temp), 1, fn);
 	}
 
 	fclose(fn);
-	// printf("Output: %s was output successfully (reverse, no bitflip)\n ",names);
 	free(names);
 
 }
 
-
+// this function deals with the directory if present
 void dir_main(char* dir_name, int max, int r_and_b, int reverse){
 	struct dirent *pDirent;
 	int size;
@@ -376,32 +376,28 @@ void dir_main(char* dir_name, int max, int r_and_b, int reverse){
 	char* dir_name_and_file;
 	int i, count=0;
 	char* buf;
+	int come_from_dir = 1;
 
 	if(dir == NULL) {
 		printf("Cannot open directory '%s'\n", dir_name);
 	}
+	// loop through the files in the opened directory
 	while((pDirent = readdir(dir)) != NULL) {
 		if (pDirent->d_name[0] == '.'){
 			continue;
 		} else if (strlen(pDirent->d_name) > 4 && pDirent->d_name[strlen(pDirent->d_name)-4] != '.' && pDirent->d_name[strlen(pDirent->d_name)-3] != 't' && pDirent->d_name[strlen(pDirent->d_name)-2] != 'x' && pDirent->d_name[strlen(pDirent->d_name)-1] != 't') {
 			continue;
 		}
-
-	
-		// size = get_file_size();
+		// allocate respective amount of memory
+		printf("original file name %s\n", pDirent->d_name);
 		int sum = strlen(dir_name) + strlen(pDirent->d_name) + 3;
-		printf("SUM: %d\n", sum);
 		dir_name_and_file = (char*)calloc(sum,sizeof(char));
 		
-
-		// debugging section
-		//printf(" size of malloc %ld, dir_name len %ld, file_name len %ld\n", sizeof(dir_name_and_file), strlen(dir_name), strlen(pDirent->d_name));
-		//printf("%s\n",dir_name_and_file);
-		// debugging section
+		// add respective ending
 		dir_name_and_file[0] = '.';
 		dir_name_and_file[1] = '/';
 		count = 2;
-
+		// create char * that contains ./directory_name/file_name
 		for(i=0; i<strlen(dir_name); i++){
 			dir_name_and_file[count] = dir_name[i];
 			count++;
@@ -411,38 +407,36 @@ void dir_main(char* dir_name, int max, int r_and_b, int reverse){
 		
 		for(i=0; i<strlen(pDirent->d_name); i++){
 			dir_name_and_file[count] = pDirent->d_name[i];
-			// printf("%s\n", dir_name_and_file);
 			count++;
 		}
 		
-		// printf("%s\n", dir_name_and_file);		
 
-		// have path as a file pointer
 		fp = fopen(dir_name_and_file, "rb");
+		// get size of file 
 		size = get_file_size(fp);
 		fclose(fp);
-		free(dir_name_and_file);
+		// compare it with max
 		if(size >= max){
 			continue;
 		}
-		// just do the bit flip
-		
+		// do proper function call depending on flags	
 	  buf = (char *)malloc((size+1) * sizeof(char));
 		if(r_and_b == 0 && reverse == 0){
-			printf("sending file from dirrectory to printregular: %s\n", dir_name_and_file);
-			print_regular(dir_name_and_file, buf);
+			print_regular(dir_name_and_file, buf, come_from_dir);
 		}
 
 		// just do the reverse
 		else if(reverse == 1){
-			print_reverse_noflip(dir_name_and_file, buf);
+			print_reverse_noflip(dir_name_and_file, buf, come_from_dir);
 		}
 		// do both reverse and bit flip
 		else if(r_and_b == 1){
-			print_reverse(dir_name_and_file, buf);
+			print_reverse(dir_name_and_file, buf, come_from_dir);
 		}
-		printf("[%s], size: %d\n", pDirent->d_name, size);	
+		// printf("[%s], size: %d\n", pDirent->d_name, size);	
 	}
 	closedir(dir);
+	free(dir_name_and_file);
+	free(buf);
 	
 }
