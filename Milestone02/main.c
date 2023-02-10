57a4f20d5ebf41d9d81a02affc1154f8e95302a5
Mitchell Trossen <mtrosse2@nd.edu>
2023-02-09 23:37:09 -0500

    Done

--- Milestone02/main.c
+++ Milestone02/main.c
@@ -2,42 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-//Declare global parent id
-pid_t pid;
+// Declare global parent id
 
 //Define functions
 void event_handler(int idk);
 
 int main(int argc, char* argv[]) {
 	if(argc > 1){
-		printf("Error: too many arguments\n");
-		return 1
+		printf("Error: too many arguments!!!!!!\n");
+		return 1;
 	}
-	//Get the parent id
-	pid = getpid();
-	char command[1000];
 
 	//Handle control-c
 	signal(SIGINT, event_handler);
 
+	char command[1000];
+	printf("Execute?????");
+	fgets(command, 1000, stdin);
+
+
 	//Tread the process
 	int cpid = fork();
-
+	
+	if(cpid < 0){
+		printf("Error: Fork Failed\n");
+		return 2;
+	}
+	
 	//Have the child ask for input and execute the command so the parent function can handle control-c
-	if (cpid == 0){
-		printf("Execute? ");
-		fgets(command, 1000, stdin);
-		system(command);
-		return 0;
+	else if (cpid == 0){
+		char* new_command = strtok(command, "\n");
+		strcpy(command, new_command);
+		// need to initialize to NULL
+		char* arguments[1000];
+		for(int i = 0; i < 1000; i++){
+			arguments[i] = NULL;
+		}
+		
+
+		char* token;
+		token = strtok(command, " ");
+
+		int i = 0;
+		while(token != NULL){
+			arguments[i] = strdup(token);
+			token = strtok(NULL, " ");
+			i = i + 1;
+		}	
+		
+		execvp(arguments[0], arguments);
+		printf("Error: Execvp failed! Please enter valid command\n");
+		exit(3);
+
+	}
+	else {
+		int status;
+		if ( waitpid(cpid, &status, 0) == -1 ) {
+			perror("wait() failed");
+			exit(EXIT_FAILURE);
+		}
+		if ( status != 0 ) {
+			printf("Error: child program exited because execvp failed\n");
+		}
+		else {
+			printf("Execution Complete!\n");
+		}
 	}
 
-	//Wait for the child to finish
-	wait(NULL);
 	
-	//Notify that execution was complete and return success
-	printf("Execution complete!\n");
-	return(0);
+	return 0;
 }
 
 
@@ -55,8 +95,8 @@ void event_handler(int idk) {
 void event_handler(int sig) {
 	if(sig == SIGINT){
 		printf("\nControl-c was pressed . . . exiting\n");
-		kill(getpid() == SIGINT);
-		exit(0)
+		kill(getpid(), SIGINT);
+		exit(0);
 	
 	}
 
